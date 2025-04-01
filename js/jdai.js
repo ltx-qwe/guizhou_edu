22ca7388763e718595cac0918deee80b2296a588
ltx-qwe <3037232620@qq.com>
2025-04-01 13:52:41 +0800

    Update jdai.js

--- js/jdai.js
+++ js/jdai.js
@@ -43,3 +43,17 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+ // 平滑滚动到锚点
+ document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
